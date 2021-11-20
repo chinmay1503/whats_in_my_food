67178f6bffd0a5284c310f0c1775bd3d4ae1d80a
Chinmay Tawde <ctawde@pdx.edu>
2021-11-19 21:46:05 -0800

    # Added ToastNotifications on Compare Page

--- whats-in-my-food-spa/src/Compare.js
+++ whats-in-my-food-spa/src/Compare.js
@@ -7,68 +7,69 @@ import {
 import "../src/css/productStyles.css"
 import "./css/compareStyles.css";
 import FoodProductApi from './services/FoodProductApi';
-export class Compare extends Component {
+import ToastNotification from "./services/ToastNotification";
+import { ToastContainer, toast } from 'material-react-toastify';
+import 'material-react-toastify/dist/ReactToastify.css';
+
+class Compare extends Component {
     constructor(props){
         super(props);
         this.state={
             product1:"",
             product2:"",
-            products:[],
-            searchTerm:""
+            products:[]
         }
         this.handleChange=this.handleChange.bind(this);
         this.handleSubmit=this.handleSubmit.bind(this);
     }
-    componentDidMount(){
+
+    componentDidMount() {
         FoodProductApi.get("/api/listAllProductNames/1").then(
             (result) => {
               this.setState({
-                products:result.data.data.products
+                products: result.data.data.products
               });
             },
             (error) => {
               console.log(error);
             }
-          );
+        );
     }
-    handleChange(e){
-        const product=e.target.name;
+
+    handleChange(e) {
+        const product = e.target.name;
         this.setState({
-            [product]:e.target.value
+            [product] : e.target.value
         });
     }
-    isProductPresentInDb(pro) {
-        this.setState({
-            searchTerm:pro
-        })
-        console.log("Products list :",this.state.products);
-        if (pro !== "" && this.state.products.length > 0) {
-            let products = this.state.products.filter((product)=>product.food_name.trim().toUpperCase() === pro.trim().toUpperCase());
+
+    isProductPresentInDb(prod) {
+        if (prod !== "" && this.state.products.length > 0) {
+            let products = this.state.products.filter((product)=>product.food_name.trim().toUpperCase() === prod.trim().toUpperCase());
             return products.length > 0 ? true : false;
         }
-
     }
+    
     handleSubmit(e){
-            e.preventDefault();
-           let flag1= this.isProductPresentInDb(this.state.product1);
-           let flag2=this.isProductPresentInDb(this.state.product2);
-            this.isProductPresentInDb(this.state.product2);
-            if(flag1&&flag2){
+           let flag1 = this.isProductPresentInDb(this.state.product1);
+           let flag2 = this.isProductPresentInDb(this.state.product2);
+            if (flag1 && flag2) {
                 this.props.history.push(`/compareProducts/${this.state.product1}/${this.state.product2}`);
-            }else if(!flag1&&!flag2){
-                alert("Product1 and Product2 not found in DB search again");
-            }else if(!flag1){
-                alert("Product1 not found in DB search again");
-            }else{
-                alert("Product2 not found in DB search again");
+            } else if (!flag1 && !flag2) {
+                ToastNotification.showErrorMessage("bottom-center", "Both Products are not present in Database, Please search for a different Products");
+            } else if (!flag1){
+                ToastNotification.showErrorMessage("bottom-center", `${this.state.product1} not present in Database, Please search for a different Product`);
+            } else {
+                ToastNotification.showErrorMessage("bottom-center", `${this.state.product2} not present in Database, Please search for a different Product`);
             }
-           
+            e.preventDefault();
     }
 
     render() {
 
         return (
             <HashRouter>
+            <ToastContainer/>
             <h1 className="heading" style={{textAlign:"center"}}>Compare Products</h1>
             <div className="compareContainer">
 
