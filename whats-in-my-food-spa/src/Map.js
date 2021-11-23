25c3eb8173d8931caa6ab7634e85f4f06b643c5a
varunvjr <varun.vjr@gmail.com>
2021-11-23 12:58:37 -0800

    d3 issue resolved

--- whats-in-my-food-spa/src/Map.js
+++ whats-in-my-food-spa/src/Map.js
@@ -2,31 +2,38 @@ import React,{useRef,useEffect} from 'react'
 import { select,geoPath,geoMercator } from 'd3'
 import useResizeObserver from './useResizeObserver'
 const Map = ({data,year}) => {
-    const svgRef=useRef();
-    const wrapperRef=useRef();
-    const dimensions=useResizeObserver(wrapperRef);
-    useEffect(()=>{
-        console.log(data.features)
-        const svg=select(svgRef.current);
+    const svgRef = useRef();
+    const wrapperRef = useRef();
+    const dimensions = useResizeObserver(wrapperRef);
 
-        const {width,height}=dimensions||wrapperRef.current.getBoundingClientRect();
-        const projection=geoMercator();
-        const pathGenerator=geoPath().projection(projection)
-
-        svg
-            .selectAll(".country")
-            .data(data.features)
-            .join("path")
-            .attr("class","country")
-            .attr("d",feature=>pathGenerator(feature))
-
-    },[data,year,dimensions])
+    useEffect(() => {
+      const svg = select(svgRef.current);
+  
+      const { width, height } =
+        dimensions || wrapperRef.current.getBoundingClientRect();
+  
+      // projects geo-coordinates on a 2D plane
+      const projection = geoMercator()
+      
+      const pathGenerator = geoPath().projection(projection);
+  
+      // render each country
+      svg
+        .selectAll(".country")
+        .data(data.features)
+        .enter().append('path')
+     
+        .attr("d", feature => pathGenerator(feature));
+  
+    },[data,dimensions,year])
+  
     return (
-        <div ref={wrapperRef} style={{marginBottom:"2rem"}}>
-            <svg ref={svgRef}></svg>
-        </div>
-    )
-}
+      <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+        <svg ref={svgRef}></svg>
+      </div>
+    );
+  }
+
 
 export default Map
 
