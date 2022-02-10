5b400baf807a28ff0810fc507ec1d66787bdb6f6
varunvjr <varun.vjr@gmail.com>
2022-02-09 17:07:50 -0800

    Food Recipe Page added

--- whats-in-my-food-spa/src/App.js
+++ whats-in-my-food-spa/src/App.js
@@ -8,6 +8,7 @@ import Home from "./Home";
 import Browse from "./Browse";
 import FoodProduct from "./FoodProduct";
 import Compare from "./Compare";
+import FoodRecipe from "./FoodRecipe";
 import CompareFoodProduct from "./CompareFoodProduct";
 import CalorieWorld from "./CalorieWorld";
 class Main extends Component {
@@ -81,6 +82,7 @@ class Main extends Component {
             <NavLink exact to="/"><img className = "logo" src = "/images/logo.png" alt="logoImg"/></NavLink>
             <div className = "linkContainer">
                 <ul className="header">
+                    <li className = {this.state.worldClass}><NavLink onClick = {this.handleNavClick} to= "/foodRecipe">Food Recipe</NavLink></li>
                     <li className = {this.state.worldClass}><NavLink onClick = {this.handleNavClick} to= "/calorie_world">Calorie World</NavLink></li>
                     <li className = {this.state.compareClass}><NavLink onClick = {this.handleNavClick} to= "/compare">Compare Products</NavLink></li>
                     <li className = {this.state.browseClass}><NavLink onClick = {this.handleNavClick} to= "/browse">Browse Products</NavLink></li>
@@ -96,6 +98,7 @@ class Main extends Component {
         <div className="content">
             <Route exact path="/" component={Home}/>
             <Route exact path="/browse" component={Browse}/>
+            <Route path="/foodRecipe" component={FoodRecipe}/>
             <Route path="/product/:productName" component={FoodProduct}/>
             <Route path="/compare" component={Compare}/>
             <Route path="/calorie_world" component={CalorieWorld}/>
---
+++ whats-in-my-food-spa/src/FoodRecipe.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FoodRecipe = () => {
+  return (
+    <div>FoodRecipe</div>
+    
+  )
+}
+
+export default FoodRecipe
